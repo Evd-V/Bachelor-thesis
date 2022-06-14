ea0ffc1fa776e5c5b70e84ab7dadac9f927569be
Evan <78163054+Evd-V@users.noreply.github.com>
2022-06-14 16:30:37 +0200

    MW virial radius
    Added option to add the virial radius of the MW as function of time in the plots

--- plot_dwarfs.py
+++ plot_dwarfs.py
@@ -49,26 +49,60 @@ def plot_graphs():
     names = ("Draco", "Sculptor", "Ursa Minor", "Carina")
     colors = ("navy", "red", "magenta", "teal")
     
+        # MW virial radius
+    dwarfGal = dwarf_galaxy("Zhao", sextans.load_dict()[0], sextans.load_dict()[1], fZhao)
+    
+    interTime = interp1d(dwarfGal.time, dwarfGal.red)
+    corrZVals = interTime(ge.conv_sec_year(-timeRange))
+    
+    zhaoIntO = interp1d(dwarfGal.red, dwarfGal.virR)        # Interp for Zhao
+    zhaoIntR = ge.conv_m_kpc(zhaoIntO(corrZVals))
+    
+    timeLims = ((3.5, 4.5), (4, 7), (1.6, 3.6), (5.5, 7))
     
     # Plotting
     fig = figure(figsize=(14,7), constrained_layout=False)
     outer_grid = fig.add_gridspec(2, 2, wspace=.15, hspace=0)
-    
     axs = outer_grid.subplots()
     
-    ind = 0
+    ind = 0                                         # Index to keep track
+    
     for i, ax in np.ndenumerate(axs):
+        
+            # Zhao model
         ax.plot(adjRange[:-1], ge.conv_m_kpc(allZPos[ind][:-1]), ls="--", 
                 color=colors[ind])
-                
+        
+            # van den Bosch model
         ax.plot(adjRange[:-1], ge.conv_m_kpc(allBPos[ind][:-1]), label=names[ind], 
                 color=colors[ind])
         
-        ax.grid()
-        ax.legend(fontsize=18)
-        ax.set_xlabel(r"$t-t_0$ (Gyr)", fontsize=18)
+            # MW virial radius
+        ax.plot(adjRange, zhaoIntR, color="slateblue", ls=":")
         
-        ind += 1
+        ax.axvline(timeLims[ind][0], color="black", ls="-.")
+        ax.axvline(timeLims[ind][1], color="black", ls="-.")
+        
+            # Setting axes
+        ax.grid()
+        ax.legend(frameon=False, loc="upper right", fontsize=18)
+        
+        if ind == 0 or ind == 1:    # Very ugly solution...
+            ax.tick_params(axis="x", labelsize=18, labelcolor="white")
+            ax.tick_params(axis="y", labelsize=18, labelcolor="black")
+            
+        else:
+            ax.tick_params(axis="both", labelsize=18)
+        
+        ind += 1                                    # To the next plot
+    
+        # Setting axes and title
+    fig.supxlabel(r"$t-t_0$ (Gyr)", fontsize=18)
+    fig.supylabel(r"$r$ (kpc)", fontsize=18)
+    fig.suptitle("Dashed = Zhao (2009), solid = van den Bosch (2014)", 
+                 fontsize=18)
+    
+#     fig.savefig("galaxy_orbits.png")
     
     show()
 
