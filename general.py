470e55d8a39c8f8cf53bac4c7e4f07c466f2d347
Evan van der Voort <evanvandervoort@gmail.com>
2022-07-10 11:08:51 +0200

    Documentation general.py

--- general.py
+++ general.py
@@ -1,10 +1,23 @@
 import numpy as np
-from astropy.constants import M_sun
-from matplotlib.pyplot import figure, show
+from scipy.interpolate import interp1d
+from astropy.constants import M_sun, pc, kpc
 
 
 def matter_density(z, omegaM0=0.3089):
-    """ Calculate the matter density at redshift z """
+    """ Calculate the matter density at redshift z, assume a LCDM 
+        cosmology.
+
+        Input:
+            z:       Redshift at which the matter density is calculated 
+                     (float or numpy array).
+            omegaM0: Matter density at redshift z=0, default: 0.3089, 
+                     according to the latest results of the Planck 
+                     collaboration (float).
+        
+        Returns:
+            omegaM:  The matter density at the given redshift(s) (float or 
+                     numpy array).
+    """
     
     omegaL0 = 1 - omegaM0            # Assuming OmegaR0 = 0 (no radiation)
     power = np.power(1+z, 3)         # (1+z)^3, z can be an array of values
@@ -13,7 +26,20 @@ def matter_density(z, omegaM0=0.3089):
     return omegaM
 
 def crit_vir_dens(z, omegaM0=0.3089):
-    """ Compute the critical overdensity for virialisation (Delta_c) """
+    """ Compute the critical overdensity for virialisation (Delta_c) at 
+        given redshift(s).
+
+        Input:
+            z:       Redshift at which the matter density is calculated 
+                     (float or numpy array).
+            omegaM0: Matter density at redshift z=0, default: 0.3089, 
+                     according to the latest results of the Planck 
+                     collaboration (float).
+        
+        Returns:
+            DeltaC:  The critical overdensity for virialisation at the 
+                     input redshift(s) (float or numpy array).
+    """
     
     omegaM = matter_density(z, omegaM0=omegaM0)     # Matter density at z
     y = omegaM - 1                                  # Parameter
@@ -22,7 +48,23 @@ def crit_vir_dens(z, omegaM0=0.3089):
     return DeltaC
 
 def hubble_para(z, H0=67.66, omegaM0=0.3089):
-    """ Hubble parameter in a matter-lambda dominated Universe """
+    """ Hubble parameter as function of redshift in a LCDM cosmology.
+
+        Input:
+            z:       Redshift(s) at which the matter density is calculated 
+                     (float or numpy array).
+            H0:      The Hubble parameter at redshift z=0, default: 67.77
+                     km/s/Mpc according to the latest results of the Plank 
+                     collaboration (float).
+            omegaM0: Matter density at redshift z=0, default: 0.3089, 
+                     according to the latest results of the Planck 
+                     collaboration (float).
+        
+        Returns:
+            hubble:  The hubble parameter at the given redshift(s) (float 
+                     or numpy array).
+    
+    """
     
     omegaL0 = 1 - omegaM0                           # Dark energy
     a = 1 / (1 + z)                                 # Acceleration
@@ -32,49 +74,123 @@ def hubble_para(z, H0=67.66, omegaM0=0.3089):
     return hubble
 
 def conv_dens(dens):
-    """ Convert from kg/m^3 to M_sun/pc^3 """
-    return dens / M_sun.value * 1 / np.power(3.2408e-17, 3)
+    """ Convert density from kg/m^3 to M_sun/pc^3.
+
+        Input:      dens:   density in kg/m^3 (float or numpy array).
+        Returns:    density in M_sun/pc^3 (float or numpy array).
+    """
+    return dens / M_sun.value * np.power(pc.value, 3)
 
 
 def conv_inv_dens(dens):
-    """ Convert from M_sun/pc^3 to kg/m^3 """
-    return dens * M_sun.value * np.power(3.2408e-17, 3)
+    """ Convert density from M_sun/pc^3 to kg/m^3 
+
+        Input:      dens:   density in M_sun/pc^3 (float or numpy array).
+        Returns:    density in kg/m^3 (float or numpy array).
+    """
+    return dens * M_sun.value * np.power(pc.value, -3)
 
 def conv_year_sec(time):
-    """ Convert years to seconds """
+    """ Convert years to seconds.
+
+        Input:      time:   time in years (float or numpy array).
+        Returns:    time in seconds (float or numpy array).
+    """
     return time * 3600 * 24 * 365.25
 
-
 def conv_sec_year(time):
+    """ Convert seconds to years.
+    
+        Input:      time:   time in seconds (float or numpy array).
+        Returns:    time in years (float or numpy array).
+    """
     return time / (3600 * 24 * 365.25)
 
-
-def conv_m_kpc(distance):
-    """ Convert meters to kpc """
-    return distance * 3.2408e-20
+def conv_m_kpc(dist):
+    """ Convert meters to kpc.
+    
+        Input:      dist:   distance in meters (float or numpy array).
+        Returns:    distance in kpc (float or numpy array).
+    """
+    return dist / kpc.value
 
 def conv_kpc_m(distance):
-    """ Convert kpc to meters """
-    return distance / 3.2408e-20
+    """ Convert kpc to meters.
+    
+        Input:      dist:   distance in kpc (float or numpy array).
+        Returns:    distance in meters (float or numpy array).
+    """
+    return distance * kpc.value
 
 
 def diff_func(y, t):
-    """ Differentiate a function """
+    """ Differentiate a function f(t) w.r.t. t
+    
+        Input:
+            y:      y values of function (numpy array).
+            t:      time values at which the function is defined 
+                    (numpy array).
+
+        Returns:
+            Differentiated function values (numpy array).
+    """
     return (y[1:] - y[:-1]) / (t[1:] - t[:-1])
 
 
 def find_closest(array, value):
-    """ Find closest element to value in array """
+    """ Find closest element to value in array.
+
+        Input:
+            array:      array with the values (numpy array).
+            value:      value to which closest element in array will be 
+                        determined (float or integer).
+        
+        Returns:
+            ind:        index of closest element in array (integer).
+            array[ind]: value of closest element in array (float or integer).
+    """
     ind = (np.abs(array - value)).argmin()
     return ind, array[ind]
 
 
 def lin_func(x):
+    """ Linear function of x. """
     return x
+
+def find_m_z(redRange, massRange, redValue):
+    """ Interpolate the virial mass for a given redshift value.
+    
+        Input:
+            redRange:   redshift range at which the mass is defined 
+                        (numpy array).
+            massRange:  mass values corresponding to redshifts (numpy array).
+            redValue:   redshift at which mass will be interpolated (float).
+        
+        Returns:
+            massAtz:    interpolated mass at redValue (float).
+    """
+    
+    interpMass = interp1d(redRange, massRange)              # Interpolation
+    massAtz = interpMass(redValue)                          # Mass at z
+    
+    return massAtz
 
 
 def runga_kuta_solver(func, h, t0, y0, *args):
-    """ Runga kuta solver for differential equations """
+    """ Runge-kuta solver for first order differential equations. 
+
+        Input:
+            func:   Function that has to be integrated, with as first 
+                    argument time, as second the integration variable 
+                    (e.g. f(t, y)) (function).
+            h:      Step size for iterative scheme (float).
+            t0:     Initial time (float).
+            y0:     Initial value of y at time t=i (float).
+            *args:  Arguments passed to the function.
+    
+        Returns:
+            yN:     The value of y at time t=i+1 (float).
+    """
     
     k1 = func(t0, y0, *args)                            # First coefficient
     
@@ -93,9 +209,22 @@ def runga_kuta_solver(func, h, t0, y0, *args):
 
 
 def execute_solver(func, h, tLim, y0, *args):
-    """ Execute the Runga kuta solver """
+    """ Use te Runge kuta method to solve ordinary differential equations.
+
+        Input:
+            func:   Function that has to be integrated, with as first 
+                    argument time, as second the integration variable 
+                    (e.g. f(t, y)) (function).
+            h:      Step size for iterative scheme (float).
+            tLim:   start and end times of integration (tuple or list).
+            y0:     Initial value of y at starting time (float).
+            *args:  Arguments passed to the function.
     
-    tNow = tLim[0]                                      # Lower time boundary
+        Returns:
+            tRange:     The time range at which the integration is done 
+                        (numpy array).
+            yResults:   The solved equation values (numpy array).
+    """
     
     tRange = np.arange(tLim[0], tLim[1]+h, h)           # Range of time values
     yResults = [y0]                                     # List with results
